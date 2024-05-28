6f2ddd323862fc66a366fca70f80d22d3495f14a
zablon <zablonoigo51@gmail.com>
2024-05-28 23:17:33 +0300

    Adding model image

--- backend/image/models.py
+++ backend/image/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    img=models.ImageField(upload_to="images")
+    rmbg_img=models.ImageField(upload_to="images_rmbg")
+
+    def __str__(self):
+        return str(self.id)
+    
