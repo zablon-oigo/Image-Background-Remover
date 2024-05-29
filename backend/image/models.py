2652489dc9ffa1e46f3b5ba2e2badae0532e2596
zablon <zablonoigo51@gmail.com>
2024-05-29 18:16:02 +0300

    Adding segmentor

--- backend/image/models.py
+++ backend/image/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 import PIL
+from io import BytesIO
 import numpy as np
-
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
 class Image(models.Model):
     img=models.ImageField(upload_to="images")
     rmbg_img=models.ImageField(upload_to="images_rmbg")
@@ -12,6 +13,7 @@ class Image(models.Model):
     def save(self, *args, **kwargs):
         pil_img=PIL.Image.open(self.img)
         img=np.array(pil_img)
+        segmentor=SelfiSegmentation()
         rmbg=segmentor.removeBG(img, (0,255,0), threshold=0.4)
         buffer=BytesIO()
         output_img=PIL.Image.fromarray(rmbg)
