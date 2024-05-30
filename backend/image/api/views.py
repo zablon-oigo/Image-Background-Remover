65c8318260a98cff83d169b1e469a2b852cfc151
zablon <zablonoigo51@gmail.com>
2024-05-30 15:35:53 +0300

    Adding action decrorator method

--- backend/image/api/views.py
+++ backend/image/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from .serializers import ImageSerializer
 from ..models import Image
-
+from rest_framework.decorators import action
 class ImageViewSet(viewsets.ModelViewSet):
     queryset=Image.objects.all()
     serializer_class=ImageSerializer
+
+    @action(methods=['GET'], detail=True)
+    def download(self, request, pk):
+        pass
